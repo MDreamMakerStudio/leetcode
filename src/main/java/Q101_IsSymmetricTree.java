b342338f8a17022d6fdc174d1c96901a563e6dc2
michaelzhu <michaelzhu@webank.com>
2019-08-19 13:50:32 +0800

    222, 101

---
+++ src/main/java/Q101_IsSymmetricTree.java
@@ -0,0 +1,108 @@
+
+
+/**
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其层次遍历结果：
+ * <p>
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1:
+ *
+ * 输入:       1         1
+ *           / \       / \
+ *          2   3     2   3
+ *
+ *         [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入:      1          1
+ *           /           \
+ *          2             2
+ *
+ *         [1,2],     [1,null,2]
+ *
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入:       1         1
+ *           / \       / \
+ *          2   1     1   2
+ *
+ *         [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/same-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Q101_IsSymmetricTree {
+    boolean flag = true;
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null && q != null) {
+            flag = false;
+            return false;
+        }
+
+        if (p != null && q == null) {
+            flag = false;
+            return false;
+        }
+
+        if(p.val != q.val) {
+            flag = false;
+            return false;
+        }
+        if(flag) {
+            isSameTree(p.left, q.left);
+            isSameTree(p.right, q.right);
+        }
+
+        return flag;
+    }
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
---
+++ src/main/java/Q222_CountTreeNodes.java
@@ -0,0 +1,105 @@
+/**
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Q222_CountTreeNodes {
+    boolean flag = true;
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        TreeNode leftTreeNode = new TreeNode(root.val);
+        leftTreeNode.left = root.left;
+        TreeNode rightTreeNode = new TreeNode(root.val);
+        rightTreeNode.right = root.right;
+        inverseRightTree(rightTreeNode);
+
+        return isSameTree(leftTreeNode, rightTreeNode);
+    }
+
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null && q != null) {
+            flag = false;
+            return false;
+        }
+
+        if (p != null && q == null) {
+            flag = false;
+            return false;
+        }
+
+        if(p.val != q.val) {
+            flag = false;
+            return false;
+        }
+        if(flag) {
+            isSameTree(p.left, q.left);
+            isSameTree(p.right, q.right);
+        }
+
+        return flag;
+    }
+
+    private void inverseRightTree(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        inverseRightTree(root.left);
+        inverseRightTree(root.right);
+        swap(root);
+    }
+
+    private void swap(TreeNode root) {
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
